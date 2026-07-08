656c73569ea3962af24ae1f61812ba0b6fc0f5f0
Sharaf AlFaqeeh <sharafalfaqeehforai@gmail.com>
2026-07-08 13:32:50 +0300

    last

--- services/frontend_service/src/main.jsx
+++ services/frontend_service/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
+import './responsive.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
